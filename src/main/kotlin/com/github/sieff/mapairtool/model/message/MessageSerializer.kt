c8c87609e620baa64325afe722eb1db64c7748cb
Steffen Schubert <steff.schubert@gmx.de>
2024-08-16 19:15:27 +0200

    Quick reactions from agent

---
+++ src/main/kotlin/com/github/sieff/mapairtool/model/message/MessageSerializer.kt
@@ -0,0 +1,19 @@
+package com.github.sieff.mapairtool.model.dataPacket
+
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+
+object DataPacketSerializer {
+    private val module = SerializersModule {
+        polymorphic(DataPacket::class) {
+            subclass(UpdateMessagesPacket::class, UpdateMessagesPacket.serializer())
+            subclass(UpdateTemporaryMessagePacket::class, UpdateTemporaryMessagePacket.serializer())
+            subclass(RequestTextInputFocusPacket::class, RequestTextInputFocusPacket.serializer())
+        }
+    }
+
+    val json: Json = Json {
+        serializersModule = module
+    }
+}
